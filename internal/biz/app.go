e61abd0e3d88f1cf0073adb5e1fd1a1ce14c6529
wangjiabao <840028401@qq.com>
2026-03-04 21:46:30 +0800

    ok

--- internal/biz/app.go
+++ internal/biz/app.go
@@ -4943,7 +4943,7 @@ func (ac *AppUsecase) StakeGetPlay(ctx context.Context, address string, req *pb.
 		}
 
 		return &pb.StakeGetPlayReply{Status: "ok", PlayStatus: 1, Amount: tmpGit}, nil
-	} else { // 输：下注金额加入池子
+	} else {                                                         // 输：下注金额加入池子
 		if err = ac.tx.ExecTx(ctx, func(ctx context.Context) error { // 事务
 			err = ac.userRepo.SetStakeGetPlaySub(ctx, user.ID, float64(req.SendBody.Amount))
 			if nil != err {
@@ -5032,6 +5032,10 @@ func (ac *AppUsecase) AdminSetLockUse(ctx context.Context, req *pb.AdminSetLockR
 		return &pb.AdminSetLockReply{
 			Status: "锁定错误",
 		}, nil
+	}
+
+	if 1 == req.OnlyAddress {
+		return &pb.AdminSetLockReply{Status: "ok"}, nil
 	}
 
 	// 推荐
